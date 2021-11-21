4ade6763b1c44f443af34ebdb6f6b60fa25871a8
Tarudahat <taruzahat@gmail.com>
2021-11-21 02:30:43 +0100

    finish lvl layout, polished stuff

--- objects/Trash.gd
+++ objects/Trash.gd
@@ -1,11 +1,14 @@
 extends Sprite
 
 func _ready():
-	texture=load("res://assets/garbage/trash_"+str(int(rand_range(0,4.5)))+".png")
+	texture=load("res://assets/garbage/trash_"+str(int(rand_range(0,3.5)))+".png")
 	pass
 
 func _on_Area2D_body_entered(body):
 	if body is KinematicBody2D:
 		if body.name == "Player":
 			body.trash+=1
+			var mouth_particles = body.get_node("subbody/Particles2D")
+			mouth_particles.modulate=Color(0.30,1,1)
+			mouth_particles.emitting = true
 			queue_free()
--- objects/player.gd
+++ objects/player.gd
@@ -1,15 +1,15 @@
 extends KinematicBody2D
 
 var velocity:Vector2
-var trash:int
+var trash:int = 0
 var hp:int = 3
 export var movement_speed:int = 350
 onready var basement = $UI.get_children()
-
+onready var max_trash = get_parent().get_node("Trash").get_child_count()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	velocity = Vector2()
+	velocity -= velocity*0.07
 
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		velocity =  get_local_mouse_position().normalized()*movement_speed
@@ -20,11 +20,13 @@ func _process(_delta):
 		$subbody.look_at(get_global_mouse_position())
 		
 	velocity = move_and_slide(velocity)
-	
+	$UI/Label.text = str(trash)+"/"+str(max_trash)
 
 
 func _on_mouth_body_entered(body):
 	if body is KinematicBody2D:
+		$subbody/Particles2D.modulate=Color(1,1,1)
+		$subbody/Particles2D.emitting = true
 		if "Fish" in body.name:
 			hp-=1
 		elif "Hart_item" in body.name:
